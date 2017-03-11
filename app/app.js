4e24ab09179b41971f535ea90f5a5106282a08ec
William A Sims III <willsims14@gmail.com>
2017-03-10 18:31:49 -0600

    Setup angular skeleton

--- app/app.js
+++ app/app.js
@@ -4,6 +4,28 @@ console.log("MAIN");
 
 var app = angular.module("MyApp", ['ngRoute']);
 
+
+app.config(function($routeProvider){
+    $routeProvider.
+    when('/', {
+        controller: 'UserCtrl'
+    }).
+    when('/profile', {
+    	templateUrl: 'partials/profile.html',
+    	controller: 'UserCtrl'
+    }).
+    when('/register', {
+    	templateUrl: 'partials/register.html',
+    	controller: 'UserCtrl'
+    }).
+    otherwise('/');
+});
+
+
+
+
+
+
 app.run(($location, FBCreds) => {
 	console.log("App.run");
     let creds = FBCreds;
